1577d4acd5b538b735a34c40040b5ece7c9f8678
hrc48 <hrc48@uclive.ac.nz>
2024-12-24 00:03:33 +1300

    created redux counter component and page. Need to convert to Client Component.

---
+++ music-sharing-frontend/src/app/counter/page.tsx
@@ -0,0 +1,11 @@
+import Counter from "../components/Counter";
+
+
+export default function CounterPage() {
+  return (
+    <div>
+      <h1>Counter Page</h1>
+      <Counter />
+    </div>
+  );
+}
